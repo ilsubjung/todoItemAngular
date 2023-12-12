62d764fafbc29e2de8016a76ad07cb9972aaee09
ilsubjung1 <ilsubjung1@nextgen.com>
2023-12-12 10:52:34 -0500

    routing moved to app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule, Routes } from '@angular/router';
 import { CreateTodoitemComponent } from './create-todoitem/create-todoitem.component';
 import { FormsModule } from '@angular/forms';
 import { UpdateTodoitemComponent } from './update-todoitem/update-todoitem.component';
 import { TodoitemDetailsComponent } from './todoitem-details/todoitem-details.component';
 
-const routes: Routes = [
-  { path: 'todoitems', component: TodoListComponent},
-  { path: 'create-todo', component: CreateTodoitemComponent},
-  { path: 'update-todo/:id', component: UpdateTodoitemComponent},
-  { path: 'detail-todo/:id', component: TodoitemDetailsComponent},
-  { path: '', redirectTo: 'todoitems', pathMatch: 'full'}
-];
 
 @NgModule({
   declarations: [
@@ -31,7 +23,6 @@ const routes: Routes = [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    RouterModule.forRoot(routes),
     FormsModule
   ],
   providers: [],
